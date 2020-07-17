767e32e0de08cedebfdcae8ea144af222ab43663
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-07-17 15:51:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,8 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = Nokogiri::HTML(open(index_url))
-    student_info = html.css('div student-card')
+    student_info = html.css('div.student-card')
+    student_info
   end
 
   def self.scrape_profile_page(profile_url)
