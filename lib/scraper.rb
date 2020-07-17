26d0ecf94566c6cc7026b88803f08ac33911ecef
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-07-17 16:10:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,20 @@ class Scraper
     html = Nokogiri::HTML(open(index_url))
     student_info = html.css('div.student-card')
     student_info.map do |s|
-      {:name s.css('div.card-text-container h4.student-name').text, 
-      :location s.css('div.card-text-container p.student-location').text, 
-      :profile_url s.css('a')[0]['href']}
+      {name: s.css('div.card-text-container h4.student-name').text, 
+      location: s.css('div.card-text-container p.student-location').text, 
+      profile_url: s.css('a')[0]['href']}
+    end
+  end
+  
+  def self.scrape_index_page(index_url)
+    html = Nokogiri::HTML(open(index_url))
+    student_info = html.css('div.student-card')
+    student_info.map do |s|
+      {name: s.css('div.card-text-container h4.student-name').text,
+      location: s.css('div.card-text-container p.student-location').text,
+      profile_url: s.css('a')[0]['href']
+      }
     end
   end
 
